41793f5e39b335cee115595618c699dc65b306c5
rachmad_sukri <rachmadsukri@student.telkomuniversity.ac.id>
2021-03-27 17:09:02 +0700

    about page

---
+++ app/src/main/java/org/d3if4025/assesment/ui/AboutPage.kt
@@ -0,0 +1,6 @@
+package org.d3if4025.assesment.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if4025.assesment.R
+
+class AboutPage : Fragment(R.layout.about_page)
--- app/src/main/java/org/d3if4025/assesment/ui/MainPage.kt
+++ app/src/main/java/org/d3if4025/assesment/ui/MainPage.kt
@@ -1,12 +1,13 @@
 package org.d3if4025.assesment.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import org.d3if4025.assesment.R
 import org.d3if4025.assesment.databinding.MainPageBinding
 
 class MainPage : Fragment() {
@@ -19,6 +20,7 @@ class MainPage : Fragment() {
     ): View? {
         binding = MainPageBinding.inflate(layoutInflater, container, false)
         binding.cek.setOnClickListener { cek() }
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -37,5 +39,18 @@ class MainPage : Fragment() {
             }
             Toast.makeText(context, jenis, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about){
+            findNavController().navigate(R.id.action_mainPage2_to_aboutPage)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
